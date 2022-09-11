1056c8b7261c42860c7ffe97c06fb9723d5e3f14
Everton-Brito <evertonbrito077@gmail.com>
2022-09-11 19:06:29 -0300

    Atualizando projeto

--- SistemaFuncionarios.Api/Program.cs
+++ SistemaFuncionarios.Api/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Configuração do CORS
+
+builder.Services.AddCors(
+    s => s.AddPolicy("DefaultPolicy", builder =>
+    {
+        builder.AllowAnyOrigin() //qualquer servidor de origem pode acessar a API
+               .AllowAnyMethod() //qualquer método (POST, PUT, DELETE, GET)
+               .AllowAnyHeader(); //qualquer parâmetro de cabeçalho (HEADER)
+    })
+);
+
+#endregion
 
 var app = builder.Build();
 
@@ -16,8 +30,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+#region habilitar o CORS
+
+app.UseCors("DefaultPolicy");
+
+#endregion
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+
